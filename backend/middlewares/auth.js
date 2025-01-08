daa5157f70f0805031f79cf05568fa90efb04404
Sujitkumar Mourya <mouryasujit04@gmail.com>
2025-01-09 02:56:59 +0530

    Adding the links for backend

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,7 +5,7 @@ import jwt from "jsonwebtoken";
 
 export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
-  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  // const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
   req.user = await User.findById("677ea2c2f2aa5a7944b935ca");
   next();
 });
